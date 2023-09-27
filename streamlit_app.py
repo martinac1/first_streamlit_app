c9466f1ec823f1e33b726b48d1e8c6d864fcecc8
martinac1 <110456653+martinac1@users.noreply.github.com>
2023-09-27 11:38:32 +0100

    Update streamlit_app.py
    added emojis

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 
 streamlit.title("Pokepedia")
 streamlit.header("First generation starters")
-streamlit.text("Pikachu*")
-streamlit.text("Charmander")
-streamlit.text("Bulbasaur")
-streamlit.text("Squirtle")
+streamlit.text("🌩️ Pikachu*")
+streamlit.text("🔥 Charmander")
+streamlit.text("🍀 Bulbasaur")
+streamlit.text("💧 Squirtle")
