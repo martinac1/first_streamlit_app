6e51c0188272c6b1709e773db28e59494e0a39c5
martinac1 <110456653+martinac1@users.noreply.github.com>
2023-09-27 11:41:50 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit
+import pandas
 
 streamlit.title("Pokepedia")
 streamlit.header("First generation starters")
@@ -6,3 +7,7 @@ streamlit.text("🌩️ Pikachu*")
 streamlit.text("🔥 Charmander")
 streamlit.text("🍀 Bulbasaur")
 streamlit.text("💧 Squirtle")
+
+streamlit.title("Smoothie table")
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
