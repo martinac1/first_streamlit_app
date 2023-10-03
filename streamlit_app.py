2eaf0cc5aee1ba4392a8489f19a8de903e9e22f7
martinac1 <110456653+martinac1@users.noreply.github.com>
2023-10-03 14:32:37 +0100

    Update streamlit_app.py
    separated url

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,7 +24,7 @@ streamlit.dataframe(fruits_to_show)
 
 # New section to display fruityvice api response
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
 streamlit.header("Fruityvice Fruit Advice!")
 
 # normalise into pandas dataframe
